e9b18c626965c183289d006c68642d7485a65296
sherloque-W <lingechettyr@ufl.edu>
2023-02-28 22:56:32 -0500

    feat: prefix hash tree conversion test, also added model function to app api

--- src/app.py
+++ src/app.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI, APIRouter, HTTPException
 import Trie
 import uvicorn
 import pickle
+from transformers import pipeline, DistilBertTokenizerFast, DistilBertForMaskedLM
+from transformers import AutoTokenizer, AutoModelForMaskedLM
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
 app = FastAPI()
 testInit = Trie.Trie_dict()
@@ -29,6 +32,17 @@ async def update(word: str, newDesc: str):
     testInit.update(word, newDesc)
     return {"word": word, "new description": newDesc}
 
+@app.get('/model/{word}')
+def model(word: str):
+    question = f'What could "{word}" mean?'
+    tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
+    model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
+    input_ids = tokenizer.encode(question + tokenizer.eos_token, return_tensors='pt')
+    chat_response = model.generate(input_ids=input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+
+    chat_response = tokenizer.decode(chat_response[0], skip_special_tokens=True)
+    chat_response = chat_response.replace(question, "")
+    return chat_response
 
 def main():
     uvicorn.run(app, host='127.0.0.1', port=8000)
